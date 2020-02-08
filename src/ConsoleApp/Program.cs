a882d5fa780827da179d08dc29be0c429e191d86
Zbigniew Kosior <3242990+zkosior@users.noreply.github.com>
2020-02-08 01:30:13 +0000

    Added command line handling

--- src/ConsoleApp/Program.cs
+++ src/ConsoleApp/Program.cs
@@ -1,13 +1,43 @@
 using System;
+using System.CommandLine;
+using System.CommandLine.Invocation;
 using System.Threading.Tasks;
 
 namespace HackerNewsScrapper.ConsoleApp
 {
 	class Program
 	{
-		static async Task Main(string[] args)
+		static async Task Main(params string[] args)
 		{
-			var nodes = await new HackerNewsScrapper().DownloadPosts(100);
+			RootCommand root = new RootCommand("Scrapps Hacker News page for posts metadata.")
+			{
+				new Option(
+					new string[] { "--posts", "-p" },
+					"How many posts to print. A positive integer <= 100.")
+				{
+					Argument = new Argument<int>(),
+				},
+			};
+
+			root.Handler = CommandHandler.Create<int>(DownloadAndPrint);
+			await root.InvokeAsync(args);
+		}
+
+		private static async Task DownloadAndPrint(int posts = 0)
+		{
+			if (posts <= 0)
+			{
+				Console.WriteLine("Nothing to download.");
+				return;
+			}
+
+			if (posts > 100)
+			{
+				Console.WriteLine("Too much to download. Limiting to 100.");
+				posts = 100;
+			}
+
+			var nodes = await new HackerNewsScrapper().DownloadPosts(posts);
 
 			Console.WriteLine(nodes);
 		}
